527e8b3766a334fb5df0760f6461210397f1ba4f
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-11-13 17:22:45 +0530

    Rector

---
+++ GenericsProblem/GenericsProblem/GenericsM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    internal class GenericsM
+    {
+        public static T MaxIntNumber<T>(T firstNum, T secondNum, T thirdNum) where T : IComparable
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0 || firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0 || firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0)
+            {
+                return firstNum;
+            }
+
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0 || secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0 || secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0)
+            {
+                return secondNum;
+            }
+
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0 || thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(secondNum) > 0 || thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) >= 0)
+            {
+                return thirdNum;
+            }
+            return firstNum;
+        }
+    }
+}
---
+++ GenericsProblem/GenericsProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
